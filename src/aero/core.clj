3075f7e39e73055884e0e1753a3407a427a448f2
Malcolm Sparks <malcolm@juxt.pro>
2015-07-06 14:09:28 +0100

    env can take a vector, implying a default

--- src/aero/core.clj
+++ src/aero/core.clj
@@ -3,10 +3,13 @@
 (ns aero.core
   (:require
    [clojure.edn :as edn]
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [schema.core :as s]))
 
 (defn readers [profile]
-  {'env (fn [x] (System/getenv (str x)))
+  {'env (fn [x]
+          (cond (vector? x) (or (System/getenv (str (first x))) (second x))
+                :otherwise (System/getenv (str x))))
    'cond (fn [m]
            (cond (contains? m profile) (clojure.core/get m profile)
                  (contains? m :default) (clojure.core/get m :default)
