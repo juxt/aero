9933f40e83f6cbbda06a0a299a8c9620657c801f
Malcolm Sparks <malcolm@juxt.pro>
2015-12-15 09:19:21 +0000

    Update schema dependency

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 ;; Copyright © 2015, JUXT LTD.
 
-(defproject aero "0.1.3"
+(defproject aero "0.1.4"
   :description "Light and fluffy configuration"
   :url "http://github.com/juxt/aero"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[prismatic/schema "0.4.3"]]
+  :dependencies [[prismatic/schema "1.0.3"]]
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}})
