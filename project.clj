4ca7b5a083f84f85e64a6812d49661fe11ff77be
Malcolm Sparks <malcolm@juxt.pro>
2017-12-01 15:45:16 +0000

    Releasing 1.1.3

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 ;; Copyright © 2015, JUXT LTD.
 
-(defproject aero "1.1.2"
+(defproject aero "1.1.3"
   :description "A small library for explicit, intentful configuration."
   :url "http://github.com/juxt/aero"
   :license {:name "The MIT License"
